544a24eee850808586033fbd4ca2c13f05263af8
Erik Andreasson <aner1797@student.ju.se>
2018-10-03 13:52:10 +0200

    Added DateTimeTests

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,36 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		DateTime date = new DateTime(2018,05,14,15,25,40);
+		
+		String stringDate = date.toString();
+		
+		
+		assertEquals(stringDate, "2018-05-14 15:25:40");
+
+	}
+
+	@Test
+	public void testDateTimeString() {
+		
+		DateTime date = new DateTime("2018-05-14 15:25:40");
+		DateTime expectedDate = new DateTime(2018,05,14,15,25,40);
+
+
+		assertEquals(date.toString(), expectedDate.toString());
+
+	}
+
+}
